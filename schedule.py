e638cd76bdc110c8e4566f04d4e5267b70b8f66d
Denis <depthinvokesu@gmail.com>
2021-09-11 13:37:43 +0300

    Added a scheduler

---
+++ schedule.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+def get_today_bdays():
+    con = sqlite3.connect('birthdaybot_db.sqlite3')
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    q = f"""
+    SELECT
+      pers_name,
+      pers_bday,
+      user_id,
+      (STRFTIME('%Y') - STRFTIME('%Y', pers_bday)) as 'age'
+    FROM
+      person
+    WHERE
+      STRFTIME('%m%d', pers_bday) = STRFTIME('%m%d')"""
+    log_msg(q)
+    cur.execute(q)
+    result = [dict(row) for row in cur.fetchall()]
+    con.commit()
+    con.close()
+    return result 
+
+def log_msg(msg):
+    print(msg)
+
+def send_msg(id, msg):
+    print(f"> id is: {id},", f"msg is: {msg}")
+
+bday_people = get_today_bdays()
+for item in bday_people:
+    id = item['user_id']
+    msg = f"Today is {item['pers_name']}'s birthday. They've turned {item['age']} years! ({item['pers_bday']})"
+    send_msg(id, msg)
