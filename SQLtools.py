272aa2128c125f5d6848a4f154e8eeb967e8c31a
Denis (linux) <depthinvokesu@gmail.com>
2022-01-20 19:39:53 +0300

    created a class with all sql functions in a separate module

---
+++ SQLtools.py
@@ -0,0 +1,116 @@
+import sqlite3
+
+class SQLtools():
+
+    def __init__(self, db_name, callback_func=print) -> None:
+        self.db_name = db_name
+        self.callback_func = callback_func
+
+
+    def select_all(self, table: str, where: dict, columns=['*']) -> list:
+
+        """SELECT [* | col1, col2...] FROM table WHERE w_key1=w_val1 AND w_key2=w_val2 ..."""
+
+        columns = ','.join(columns) # str: "col1, col2 ..."
+        where_vals = list(where.values()) # list: [w_val1, w_val2 ...]
+        where_clause = " AND ".join([f"{item}=?" for item in where.keys()]) # str: "w_key1=? AND w_key2=?"
+
+        q = f"SELECT {columns} FROM {table} WHERE {where_clause}"
+
+        con = sqlite3.connect(self.db_name)
+        con.set_trace_callback(self.callback_func)
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        self.callback_func(q)
+        cur.execute(q, where_vals)
+        result = [dict(row) for row in cur.fetchall()]
+        con.commit()
+        con.close()
+        return result
+
+    def select_one(self, table: str, where: dict, columns=['*']) -> dict:
+
+        """SELECT [* | col1, col2...] FROM table WHERE w_key1=w_val1 AND w_key2=w_val2 ..."""
+
+        columns = ','.join(columns) # str: "col1, col2 ..."
+        where_vals = list(where.values()) # list: [w_val1, w_val2 ...]
+        where_clause = " AND ".join([f"{item}=?" for item in where.keys()]) # str: "w_key1=? AND w_key2=?"
+
+        q = f"SELECT {columns} FROM {table} WHERE {where_clause}"
+
+        con = sqlite3.connect(self.db_name)
+        con.set_trace_callback(self.callback_func)
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        self.callback_func(q)
+        cur.execute(q, where_vals)
+        result = cur.fetchone()
+        con.commit()
+        con.close()
+        return dict(result) if result else None
+
+    def delete(self, table: str, where: dict):
+
+        """DELETE FROM table WHERE w_key1=w_val1 AND w_key2=w_val2 ..."""
+
+        where_vals = list(where.values()) # list: [w_val1, w_val2 ...]
+        where_clause = " AND ".join([f"{item}=?" for item in where.keys()]) # str: "w_key1=? AND w_key2=?"
+
+        q = f"DELETE FROM {table} WHERE {where_clause}"
+
+        con = sqlite3.connect(self.db_name)
+        con.set_trace_callback(self.callback_func)
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        self.callback_func(q)
+        cur.execute(q, where_vals)
+        con.commit()
+        con.close()
+
+    def insert(self, table: str, data: dict):
+
+        """INSERT INTO table (key1, key2...) VALUES (val1, val2...) """
+
+        keys = ','.join(data.keys()) # str: "key1, key2 ..."
+        vals = list(data.values()) # str: "val1, val2 ..."
+        qmarks = ','.join('?'*len(vals)) # palceholders for values, str: "?, ? ..."
+
+        q = f"INSERT INTO {table} ({keys}) VALUES ({qmarks})"
+
+        con = sqlite3.connect(self.db_name)
+        con.set_trace_callback(self.callback_func)
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        self.callback_func(q)
+        cur.execute(q, vals)
+        con.commit()
+        con.close()
+
+    def update(self, table: str, set: dict, where: dict):
+
+        """UPDATE table SET s_key1=s_val1 AND s_key2=s_val2 ... WHERE w_key1=w_val1 AND w_key2=w_val2 ..."""
+
+        set_vals = list(set.values()) # str: "s_val1, s_val2 ..."
+        set_clause = " AND ".join([f"{item}=?" for item in set.keys()]) # str: "s_key1=? AND s_key2=?"
+
+        where_vals = list(where.values()) # list: [w_val1, w_val2 ...]
+        where_clause = " AND ".join([f"{item}=?" for item in where.keys()]) # str: "key1=? AND key2=? ..."
+
+        q = f"UPDATE {table} SET {set_clause} WHERE {where_clause}"
+
+        con = sqlite3.connect(self.db_name)
+        #con.set_trace_callback(self.callback_func)
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        self.callback_func(q)
+        cur.execute(q, [*set_vals, *where_vals])
+        con.commit()
+        con.close()
+
+# ob1 = SQLtools(db_name="birthdaybot_db.sqlite3")
+
+# print(ob1.db_name)
+# # ob1.insert(table="user", data={"user_id": 2})
+# ob1.update(table="user", set={"username":"user2"}, where={"user_id":2})
+# res = ob1.select_all(table="user", where={"user_id":2})
+# print(res)
--- prototype.py
+++ prototype.py
@@ -1,10 +1,14 @@
-import sqlite3
 import re
 from datetime import datetime as dt
+from SQLtools import SQLtools
 
-db_name = 'birthdaybot_db.sqlite3'
+def log_msg(msg):
+    print(msg)
 
-m1 = {'text':'1', 'chat':{'id':13, 'username':'user13'}}
+sql = SQLtools(db_name='birthdaybot_db.sqlite3', callback_func=log_msg)
+
+
+m1 = {'text':'/all', 'chat':{'id':4, 'username':'user13'}}
 
 def wf(msg):
 
@@ -12,48 +16,52 @@ def wf(msg):
     id = msg['chat']['id']
     username = msg['chat']['username']
 
-    user_command = select_one(table='user_command', where={'user_id':id}) # {user_id: x, cmd_id: y, step_id: z}
+    user_command = sql.select_one(table='user_command', where={'user_id':id}) # {user_id: x, cmd_id: y, step_id: z}
 
     if text == '/add':
-        clear(table='user_command', where={'user_id':id})
-        clear(table='add_cache', where={'user_id':id})
-        insert(table='user_command', data={'user_id': id, 'cmd_id': 1, 'step_id': 1})
-        add(id, text, username)
+        sql.delete(table='user_command', where={'user_id':id})
+        sql.delete(table='add_cache', where={'user_id':id})
+        sql.insert(table='user_command', data={'user_id': id, 'cmd_id': 1, 'step_id': 1})
+        add_person(id, text, username)
 
     elif text == '/delete':
-        clear(table='user_command', where={'user_id':id})
-        clear(table='delete_cache', where={'user_id':id})
-        insert(table='user_command', data={'user_id': id, 'cmd_id': 2, 'step_id': 1})
-        delete(id, text)
+        sql.delete(table='user_command', where={'user_id':id})
+        sql.delete(table='delete_cache', where={'user_id':id})
+        sql.insert(table='user_command', data={'user_id': id, 'cmd_id': 2, 'step_id': 1})
+        delete_person(id, text)
 
     elif text == '/month':
         response = this_month(id)
         send_msg(id, response)
 
+    elif text == '/list' or '/all':
+        response = sql.select_all(table="person", where={"user_id":id})
+        send_msg(id, response)
+
     elif user_command: # Record with the id exists in user_command
         if user_command['cmd_id'] == 1: # command_id=1 (add) for the user_id in user_command
-            add(id, text, username)
+            add_person(id, text, username)
         elif user_command['cmd_id'] == 2: # command_id=2 (delete) for the user_id in user_command
-            delete(id, text)
+            delete_person(id, text)
         else:
-            clear(table='user_command', where={'user_id':id})
+            sql.delete(table='user_command', where={'user_id':id})
             show_start_msg(id)
     else:
         show_start_msg(id)
         
 
 
-def add(id, text, username):
+def add_person(id, text, username):
     log_msg("ADD PROCESS HAS STARTED")
    
-    step_id = select_one(table='user_command', where={'user_id':id})['step_id']
+    step_id = sql.select_one(table='user_command', where={'user_id':id})['step_id']
     log_msg(f"step id is {step_id}")
 
     if step_id == 1: # add message has come
 
         # Inserting user's data to cache table, incrementing step_id and sending a message with result
-        insert(table='add_cache', data={'user_id':id, 'username': username})
-        update(table='user_command', set={'step_id':2}, where={'user_id':id})
+        sql.insert(table='add_cache', data={'user_id':id, 'username': username})
+        sql.update(table='user_command', set={'step_id':2}, where={'user_id':id})
         send_msg(id, "Enter a person name")
 
     elif step_id == 2: # pers_name has come
@@ -63,8 +71,8 @@ def add(id, text, username):
             return
         
         # Inserting the input to the cache table, incrementing step_id and sending a message with result
-        update(table='add_cache', set={'pers_name':text}, where={'user_id':id})
-        update(table='user_command', set={'step_id':3}, where={'user_id':id})
+        sql.update(table='add_cache', set={'pers_name':text}, where={'user_id':id})
+        sql.update(table='user_command', set={'step_id':3}, where={'user_id':id})
         send_msg(id, f"OK, now enter {text}'s birthday (YYYY-MM-DD)")
 
     elif step_id == 3: # pers_bday has come
@@ -83,37 +91,37 @@ def add(id, text, username):
         date_sql = date_obj.strftime('%Y-%m-%d') # YYYY-MM-DD string DD
 
         # Inserting the input to the cache table
-        update(table='add_cache', set={'pers_bday':date_sql}, where={'user_id':id})
+        sql.update(table='add_cache', set={'pers_bday':date_sql}, where={'user_id':id})
         
         # Copying the data from cache to person table
-        pers_name = select_one(table='add_cache', where={'user_id':id})['pers_name']
-        insert(table='person', data={'pers_name':pers_name, 'pers_bday':date_sql, 'user_id':id})
+        pers_name = sql.select_one(table='add_cache', where={'user_id':id})['pers_name']
+        sql.insert(table='person', data={'pers_name':pers_name, 'pers_bday':date_sql, 'user_id':id})
 
         # Inserting user's data to user table
-        user_tb = select_one(table='user', where={'user_id': id})
+        user_tb = sql.select_one(table='user', where={'user_id': id})
         if user_tb: # record is already exists
-            update(table='user', set={'username':username}, where={'user_id':id})
+            sql.update(table='user', set={'username':username}, where={'user_id':id})
         else:
-            insert(table='user', data={'user_id':id, 'username': username})
+            sql.insert(table='user', data={'user_id':id, 'username': username})
 
         # Deleting cache and sending the final message
-        clear(table='add_cache', where={'user_id':id})
-        clear(table='user_command', where={'user_id':id})
+        sql.delete(table='add_cache', where={'user_id':id})
+        sql.delete(table='user_command', where={'user_id':id})
         send_msg(id, f"{pers_name}, born {date_frnd} has been added")
     else:
-        clear(table='add_cache', where={'user_id':id})
-        clear(table='user_command', where={'user_id':id})
+        sql.delete(table='add_cache', where={'user_id':id})
+        sql.delete(table='user_command', where={'user_id':id})
         show_start_msg(id)
 
-def delete(id, text):
+def delete_person(id, text):
 
-    step_id = select_one(table='user_command', where={'user_id':id})['step_id']
+    step_id = sql.select_one(table='user_command', where={'user_id':id})['step_id']
     log_msg(f"step id is {step_id}")
 
     if step_id == 1: # delete message has come
 
         # Composing lists of people for current user
-        person_tb = select_all(table='person', where={'user_id':id}, columns=['rowid as pers_id', '*']) # [{pers_name, pers_bday, user_id, pers_id}, ...]
+        person_tb = sql.select_all(table='person', where={'user_id':id}, columns=['rowid as pers_id', '*']) # [{pers_name, pers_bday, user_id, pers_id}, ...]
         if len(person_tb) == 0: # there is no one in person table
             send_msg(id, "There is no one to delete")
             return
@@ -129,10 +137,10 @@ def delete(id, text):
         
         # Inserting the list into delete_cache table
         for person in insert_list:
-            insert(table='delete_cache', data=person)
+            sql.insert(table='delete_cache', data=person)
 
         # Incrementing step_id and sending a message with result
-        update(table='user_command', set={'step_id': 2}, where={'user_id': id})
+        sql.update(table='user_command', set={'step_id': 2}, where={'user_id': id})
         send_msg(id, "Enter a number of a person you want to delete")
         send_msg(id, show_list)
 
@@ -144,32 +152,32 @@ def delete(id, text):
         input_num = int(text)
 
         # Checking if input number is valid
-        delete_cache_tb = select_all(table='delete_cache', where={'user_id': id}) # [{user_id, pers_id, pers_num}, ..]
+        delete_cache_tb = sql.select_all(table='delete_cache', where={'user_id': id}) # [{user_id, pers_id, pers_num}, ..]
         pers_numbers = [item['pers_num'] for item in delete_cache_tb] # [1, 2...]
         if input_num not in pers_numbers:
             send_msg(id, "Wrong number, send /delete to get a list")
             return
         
         # Getting id, name, bday of the person to delete
-        pers_id = select_one(table='delete_cache', where={'user_id': id, 'pers_num': input_num})['pers_id']
-        person = select_one(table='person', where={'rowid': pers_id}) # {pers_name, pers_bday}
+        pers_id = sql.select_one(table='delete_cache', where={'user_id': id, 'pers_num': input_num})['pers_id']
+        person = sql.select_one(table='person', where={'rowid': pers_id}) # {pers_name, pers_bday}
 
         # Deleting target person from person table and deleting cache
-        clear(table='person', where={'rowid': pers_id})
-        clear(table='delete_cache', where={'user_id':id})
-        clear(table='user_command', where={'user_id':id})
+        sql.delete(table='person', where={'rowid': pers_id})
+        sql.delete(table='delete_cache', where={'user_id':id})
+        sql.delete(table='user_command', where={'user_id':id})
 
         # Final message
         send_msg(id, f"{person['pers_name']} born {person['pers_bday']} has been deleted")
 
     else:
-        clear(table='delete_cache', where={'user_id':id})
-        clear(table='user_command', where={'user_id':id})
+        sql.delete(table='delete_cache', where={'user_id':id})
+        sql.delete(table='user_command', where={'user_id':id})
         show_start_msg(id)
 
 def this_month(id):
     month = dt.now().strftime("%m")
-    result = select_all(table='person', where={"user_id": id, "STRFTIME('%m', pers_bday)": month})
+    result = sql.select_all(table='person', where={"user_id": id, "STRFTIME('%m', pers_bday)": month})
     return result
 
 def show_start_msg(id):
@@ -201,106 +209,6 @@ def istext(text):
         return True
     else:
         return False
-
-def select_all(table: str, where: dict, columns=['*']) -> list:
-
-    """SELECT [* | col1, col2...] FROM table WHERE w_key1=w_val1 AND w_key2=w_val2 ..."""
-
-    columns = ','.join(columns) # str: "col1, col2 ..."
-    where_vals = list(where.values()) # list: [w_val1, w_val2 ...]
-    where_clause = " AND ".join([f"{item}=?" for item in where.keys()]) # str: "w_key1=? AND w_key2=?"
-
-    q = f"SELECT {columns} FROM {table} WHERE {where_clause}"
-
-    con = sqlite3.connect(db_name)
-    con.set_trace_callback(log_msg)
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    log_msg(q)
-    cur.execute(q, where_vals)
-    result = [dict(row) for row in cur.fetchall()]
-    con.commit()
-    con.close()
-    return result
-
-def select_one(table: str, where: dict, columns=['*']) -> dict:
-
-    """SELECT [* | col1, col2...] FROM table WHERE w_key1=w_val1 AND w_key2=w_val2 ..."""
-
-    columns = ','.join(columns) # str: "col1, col2 ..."
-    where_vals = list(where.values()) # list: [w_val1, w_val2 ...]
-    where_clause = " AND ".join([f"{item}=?" for item in where.keys()]) # str: "w_key1=? AND w_key2=?"
-
-    q = f"SELECT {columns} FROM {table} WHERE {where_clause}"
-
-    con = sqlite3.connect(db_name)
-    con.set_trace_callback(log_msg)
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    log_msg(q)
-    cur.execute(q, where_vals)
-    result = cur.fetchone()
-    con.commit()
-    con.close()
-    return dict(result) if result else None
-
-def clear(table: str, where: dict):
-
-    """DELETE FROM table WHERE w_key1=w_val1 AND w_key2=w_val2 ..."""
-
-    where_vals = list(where.values()) # list: [w_val1, w_val2 ...]
-    where_clause = " AND ".join([f"{item}=?" for item in where.keys()]) # str: "w_key1=? AND w_key2=?"
-
-    q = f"DELETE FROM {table} WHERE {where_clause}"
-
-    con = sqlite3.connect(db_name)
-    con.set_trace_callback(log_msg)
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    log_msg(q)
-    cur.execute(q, where_vals)
-    con.commit()
-    con.close()
-
-def insert(table: str, data: dict):
-
-    """INSERT INTO table (key1, key2...) VALUES (val1, val2...) """
-
-    keys = ','.join(data.keys()) # str: "key1, key2 ..."
-    vals = list(data.values()) # str: "val1, val2 ..."
-    qmarks = ','.join('?'*len(vals)) # palceholders for values, str: "?, ? ..."
-
-    q = f"INSERT INTO {table} ({keys}) VALUES ({qmarks})"
-
-    con = sqlite3.connect(db_name)
-    con.set_trace_callback(log_msg)
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    log_msg(q)
-    cur.execute(q, vals)
-    con.commit()
-    con.close()
-
-def update(table: str, set: dict, where: dict):
-
-    """UPDATE table SET s_key1=s_val1 AND s_key2=s_val2 ... WHERE w_key1=w_val1 AND w_key2=w_val2 ..."""
-
-    set_vals = list(set.values()) # str: "s_val1, s_val2 ..."
-    set_clause = " AND ".join([f"{item}=?" for item in set.keys()]) # str: "s_key1=? AND s_key2=?"
-
-    where_vals = list(where.values()) # list: [w_val1, w_val2 ...]
-    where_clause = " AND ".join([f"{item}=?" for item in where.keys()]) # str: "key1=? AND key2=? ..."
-
-    q = f"UPDATE {table} SET {set_clause} WHERE {where_clause}"
-
-    con = sqlite3.connect(db_name)
-    #con.set_trace_callback(log_msg)
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    log_msg(q)
-    cur.execute(q, [*set_vals, *where_vals])
-    con.commit()
-    con.close()
 
 wf(m1)
 
