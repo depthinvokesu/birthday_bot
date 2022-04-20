3d88c0cba8d7dacbb029bc7bd67a907dc124c86c
depthinvokesu <55591216+depthinvokesu@users.noreply.github.com>
2022-04-20 22:12:19 +0300

    Add files via upload

--- SQLtools.py
+++ SQLtools.py
@@ -1,9 +1,31 @@
 import sqlite3
 from typing import List, Dict, Tuple, Optional
-from settings import log_msg
+
+class Cursor:
+
+    """Context manager to work with SQLite3 connection object"""
+
+    def __init__(self, db_name, callback_func, query):
+        self.db_name = db_name
+        self.callback_func = callback_func
+        self.query = query
+    
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        self.conn.set_trace_callback(self.callback_func) # Set which func outputs logs
+        self.conn.row_factory = sqlite3.Row
+        return self.conn.cursor()
+
+    def __exit__(self, type, value, traceback):
+        self.callback_func(self.query) # Output logs
+        self.conn.commit()
+        self.conn.close()
+        
 
 class SQLtools:
 
+    """Class containing basic SQL functions e.g. SELECT, UPDATE, INSERT, DELETE, REPLACE in SQLite3 implementation"""
+    
     def __init__(self, db_name, callback_func=print):
         self.db_name = db_name
         self.callback_func = callback_func
@@ -12,105 +34,86 @@ class SQLtools:
 
         """SELECT [* | col1, col2...] FROM table WHERE w_key1=w_val1 AND w_key2=w_val2 ..."""
 
-        q, where_vals = self.__prepare_select_query(table, where, columns)
+        columns = ','.join(columns) # str: "col1, col2 ..."
+        where_vals, where_expr = self.__get_query_params(clause_data=where, clause_type='where')
 
-        cur, connection = self.__get_cursor(q)
-        cur.execute(q, where_vals)
-        result = [dict(row) for row in cur.fetchall()]
-        connection.close()
+        q = f"SELECT {columns} FROM {table} WHERE {where_expr}"
+
+        with Cursor(self.db_name, self.callback_func, q) as cur:
+            cur.execute(q, where_vals)
+            result = [dict(row) for row in cur.fetchall()]
         return result
 
     def select_one(self, table: str, where: dict, columns=['*']) -> Optional[dict]:
 
         """SELECT [* | col1, col2...] FROM table WHERE w_key1=w_val1 AND w_key2=w_val2 ..."""
 
-        q, where_vals = self.__prepare_select_query(table, where, columns)
+        columns = ','.join(columns) # str: "col1, col2 ..."
+        where_vals, where_expr = self.__get_query_params(clause_data=where, clause_type='where')
 
-        cur, connection = self.__get_cursor(q)
-        cur.execute(q, where_vals)
-        result = cur.fetchone()
-        connection.close()
+        q = f"SELECT {columns} FROM {table} WHERE {where_expr}"
+
+        with Cursor(self.db_name, self.callback_func, q) as cur:
+            cur.execute(q, where_vals)
+            result = cur.fetchone()
         return dict(result) if result else None
 
     def delete(self, table: str, where: dict) -> None:
 
         """DELETE FROM table WHERE w_key1=w_val1 AND w_key2=w_val2 ..."""
 
-        where_vals = list(where.values()) # list: [w_val1, w_val2 ...]
-        where_clause = self.__join_conditions(where) # str: "w_key1=? AND w_key2=?"
+        where_vals, where_expr = self.__get_query_params(clause_data=where, clause_type='where')
 
-        q = f"DELETE FROM {table} WHERE {where_clause}"
+        q = f"DELETE FROM {table} WHERE {where_expr}"
 
-        cur, connection = self.__get_cursor(q)
-        cur.execute(q, where_vals)
-        connection.commit()
-        connection.close()
-
-    def insert(self, table: str, data: dict) -> None:
-
-        """INSERT INTO table (key1, key2...) VALUES (val1, val2...) """
-
-        keys = ','.join(data.keys()) # str: "key1, key2 ..."
-        vals = list(data.values()) # str: "val1, val2 ..."
-        qmarks = ','.join('?'*len(vals)) # palceholders for values, str: "?, ? ..."
-
-        q = f"INSERT INTO {table} ({keys}) VALUES ({qmarks})"
-
-        cur, connection = self.__get_cursor(q)
-        cur.execute(q, vals)
-        connection.commit()
-        connection.close()
+        with Cursor(self.db_name, self.callback_func, q) as cur:
+            cur.execute(q, where_vals)
 
     def update(self, table: str, set: dict, where: dict) -> None:
 
         """UPDATE table SET s_key1=s_val1 AND s_key2=s_val2 ... WHERE w_key1=w_val1 AND w_key2=w_val2 ..."""
 
-        set_vals = list(set.values()) # str: "s_val1, s_val2 ..."
-        set_clause = ", ".join([f"{item}=?" for item in set.keys()]) # str: "s_key1=?, s_key2=? ..."
+        set_vals, set_expr = self.__get_query_params(clause_data=set, clause_type='set')
+        where_vals, where_expr = self.__get_query_params(clause_data=where, clause_type='where')
 
-        where_vals = list(where.values()) # list: [w_val1, w_val2 ...]
-        where_clause = self.__join_conditions(where) # str: "w_key1=? AND w_key2=?"
+        q = f"UPDATE {table} SET {set_expr} WHERE {where_expr}"
 
-        q = f"UPDATE {table} SET {set_clause} WHERE {where_clause}"
+        with Cursor(self.db_name, self.callback_func, q) as cur:
+            cur.execute(q, [*set_vals, *where_vals])
 
-        cur, connection = self.__get_cursor(q)
-        cur.execute(q, [*set_vals, *where_vals])
-        connection.commit()
-        connection.close()
+    def insert(self, table: str, data: dict) -> None:
+
+        """INSERT INTO table (key1, key2...) VALUES (val1, val2...) """
+
+        keys, vals, qmarks = self.__get_query_params(clause_data=data, clause_type='insert_replace')
+
+        q = f"INSERT INTO {table} ({keys}) VALUES ({qmarks})"
+
+        with Cursor(self.db_name, self.callback_func, q) as cur:
+            cur.execute(q, vals)
 
     def replace(self, table: str, data: dict) -> None:
 
         """INSERT OR REPLACE INTO table (key1, key2...) VALUES (val1, val2...) """
 
-        keys = ','.join(data.keys()) # str: "key1, key2 ..."
-        vals = list(data.values()) # str: "val1, val2 ..."
-        qmarks = ','.join('?'*len(vals)) # palceholders for values, str: "?, ? ..."
+        keys, vals, qmarks = self.__get_query_params(clause_data=data, clause_type='insert_replace')
 
         q = f"REPLACE INTO {table} ({keys}) VALUES ({qmarks})"
 
-        cur, connection = self.__get_cursor(q)
-        cur.execute(q, vals)
-        connection.commit()
-        connection.close()
-
-    def __prepare_select_query(self, table: str, where: dict, columns) -> Tuple[list]:
-        columns = ','.join(columns) # str: "col1, col2 ..."
-        where_vals = list(where.values()) # list: [w_val1, w_val2 ...]
-        where_clause = self.__join_conditions(where) # str: "w_key1=? AND w_key2=?"
-        return f"SELECT {columns} FROM {table} WHERE {where_clause}", where_vals
-
-    def __join_conditions(self, cond: dict) -> str:
-        return " AND ".join([f"{item}=?" for item in cond.keys()]) # str: "key1=? AND key2=? ..."
-
-    def __get_cursor(self, q):
-        con = sqlite3.connect(self.db_name)
-        con.set_trace_callback(self.callback_func) # Output logs
-        con.row_factory = sqlite3.Row
-        cur = con.cursor()
-        self.callback_func(q) # Output logs
-        return cur, con
+        with Cursor(self.db_name, self.callback_func, q) as cur:
+            cur.execute(q, vals)
 
 
-sql = SQLtools(db_name="birthdaybot_db.sqlite3", callback_func=log_msg)
-        
-sql.select_all("user", {1:1})
+    def __get_query_params(self, clause_data: dict, clause_type: str) -> Tuple:
+        if clause_type == 'insert_replace':
+            keys = ','.join(clause_data.keys()) # str: "key1, key2 ..."
+            vals = list(clause_data.values()) # str: "val1, val2 ..."
+            qmarks = ','.join('?'*len(vals)) # palceholders for values, str: "?, ? ..."
+            return keys, vals, qmarks
+        else:
+            if clause_type == 'where' : delim = ' AND '
+            if clause_type == 'set' : delim = ', '
+            vals = list(clause_data.values()) # str: "val1, val2 ..."
+            expr = delim.join([f"{item}=?" for item in clause_data.keys()]) # str: "key1=? AND key2=?" or "key1=?, key2=?"
+            return vals, expr
+
